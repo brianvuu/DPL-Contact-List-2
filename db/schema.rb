77c1b4c47b26d688b9ffd01d2e83651382a59fb9
Brian Vuu <brianvuu@gmail.com>
2018-08-29 02:17:24 -0600

    Updated DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_080658) do
+ActiveRecord::Schema.define(version: 2018_08_29_081100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contacts", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
     t.integer "age"
     t.string "phone"
     t.string "email"
